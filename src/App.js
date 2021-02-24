75da78f3bcfbd0dcf3c5f8316c0e9a9d0ae5cd7e
TaikiCode <metoro5151@gmail.com>
2021-02-25 01:46:35 +0900

    setup project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "./components/Container";
+import { words } from "./data/Words";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1>setup project</h1>
+        <div>
+          {words.map((word, index) => {
+            return <p key={index}>{word}</p>;
+          })}
+        </div>
+      </Container>
     </div>
   );
 }
